16cd21877e2183de8e8c85909926b72581fc41fe
Jaber Valinejad <66491004+Jaber-Valinejad@users.noreply.github.com>
2024-07-16 17:19:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='function',
     version='0.1',
-    packages=find_packages(where='src'),  # Automatically find packages in 'src'
-    package_dir={'': 'functions'},  # Specify that packages are under the 'src' directory
+    packages=find_packages(where='OrFi'),  # Automatically find packages in 'src'
+    package_dir={'': 'src'},  # Specify that packages are under the 'src' directory
     install_requires=[
         'transformers',
         'sentence-transformers',
