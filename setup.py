17771dd7c32855ac88d7a9efbd4edb8bdb3e9154
Jaber Valinejad <66491004+Jaber-Valinejad@users.noreply.github.com>
2024-07-16 16:21:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='my_project',
     version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
     install_requires=[
-'transformers',
-'sentence-transformers',
-'pandas',
-'ast',
-'itertools',
-'nltk',
-'re',
-'spacy',
-'torch',
-'tqdm',
-'time',
-'datetime',
-'random',
-'numpy',
-'math'
+        'transformers',
+        'sentence-transformers',
+        'pandas',
+        'nltk',
+        'spacy',
+        'torch',
+        'tqdm',
+        'numpy'
     ],
 )
