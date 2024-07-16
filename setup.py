3be5ea3eee8fd9d7d3f9616fa93ddffe650ad191
Jaber Valinejad <66491004+Jaber-Valinejad@users.noreply.github.com>
2024-07-16 16:25:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='my_project',
     version='0.1',
+    packages=["src"]
     install_requires=[
         'transformers',
         'sentence-transformers',
