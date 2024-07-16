187ad411dbf2d9c35fc7b3d5a9bc99305f004dd2
Jaber Valinejad <66491004+Jaber-Valinejad@users.noreply.github.com>
2024-07-16 16:29:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='my_project',
     version='0.1',
-    packages=["src"]
+    packages=find_packages(where='src'),  # Automatically find packages in 'src'
+    package_dir={'': 'src'},  # Specify that packages are under the 'src' directory
     install_requires=[
         'transformers',
         'sentence-transformers',
